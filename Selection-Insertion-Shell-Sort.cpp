b8b9aac76b7b9b9b89dd25bab853446aff1a655d
Ze Lei <57436044+CaedemSoftware@users.noreply.github.com>
2020-11-20 03:16:00 -0800

    Update Selection-Insertion-Shell-Sort.cpp

--- Selection-Insertion-Shell-Sort.cpp
+++ Selection-Insertion-Shell-Sort.cpp
@@ -3,148 +3,167 @@
 #include <vector>
 using namespace std;
 
+void printVector(vector<int>& vec) {
+	size_t n = vec.size();
+	for (std::vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+		std::cout << *i << ' ';
+	}
+	cout << "\n";
+}
+
 void selectionSort(vector<int>& vec) {
-    cout << "[Numbers in brackets] are unsorted" << endl;
-    size_t n = vec.size();
+	cout << "[Numbers in brackets] are unsorted" << endl;
+	size_t n = vec.size();
 
-    // One by one move boundary of unsorted subvec
-    for (size_t i = 0; i < n - 1; i++) {
-        // Find the minimum element in unsorted vec
-        int min = i;
-        for (size_t j = i + 1; j < n; j++) {
-            if (vec[j] < vec[min]) {
-                min = j;
-            }
-        }
+	// move boundary of unsorted subvector
+	for (size_t i = 0; i < n - 1; i++) {
+		// find the minimum element in unsorted vector
+		int min = i;
+		for (size_t j = i + 1; j < n; j++) {
+			if (vec[j] < vec[min]) {
+				min = j;
+			}
+		}
 
-        // Swap the found minimum element with the first
-        // element
-        int temp = vec[min];
-        vec[min] = vec[i];
-        vec[i] = temp;
-
-        cout << "Step " << (i + 1) << endl;
-        for (size_t k = 0; k < vec.size(); k++) {
-            cout << vec[k];
-            cout << " ";
-            if (k == i) {
-                cout << "[";
-            }
-        }
-        cout << "\b \b]" << endl;
-    }
+		cout << "*** Iteration " << (i + 1) << " ***" << endl;
+		cout << "smallest unsorted number: " << vec[min] << endl;
+		// Swap the found minimum element with the first element of unsorted vector
+		int temp = vec[min];
+		vec[min] = vec[i];
+		vec[i] = temp;
+		//print vector
+		for (size_t k = 0; k < vec.size(); k++) {
+			cout << vec[k];
+			cout << " ";
+			if (k == i) {
+				cout << "[";
+			}
+		}
+		cout << "\b \b]" << endl;
+	}
 }
 
 void insertionSort(vector<int>& vec) {
-    cout << "[Numbers in brackets] are sorted" << endl;
-    size_t n = vec.size();
-    for (size_t i = 1; i < n; i++) {
-        int key = vec[i];
-        int j = i - 1;
+	cout << "[Numbers in brackets] are sorted\nNumbers greater than inserted number are moved one index higher" << endl;
+	size_t n = vec.size();
+	for (size_t i = 1; i < n; i++) {
+		int insertee = vec[i];
+		int j = i - 1;
 
-        /* Move elements of arr[0..i-1], that are
-           greater than key, to one position ahead
-           of their current position */
-        while (j >= 0 && vec[j] > key) {
-            vec[j + 1] = vec[j];
-            j = j - 1;
-        }
-        vec[j + 1] = key;
+		/* Move elements of vector[0..i-1], that are
+		   greater than insertee, to one position ahead
+		   of their current position */
+		while (j >= 0 && vec[j] > insertee) {
+			vec[j + 1] = vec[j];
+			j = j - 1;
+		}
+		vec[j + 1] = insertee;
 
-        //print steps
-        cout << "Step " << (i) << endl;
-        cout << "[";
-        for (size_t k = 0; k < vec.size(); k++) {
-            cout << vec[k];
-            if (k == i) {
-                cout << "]";
-            }
-            cout << " ";
-        }
-        cout << "\n";
-    }
+		cout << "*** Iteration " << i << " ***" << endl;
+		cout << "Inserting: " << insertee << endl;
+		cout << "[";
+		//print vector
+		for (size_t k = 0; k < vec.size(); k++) {
+			cout << vec[k];
+			if (k == i) {
+				cout << "]";
+			}
+			cout << " ";
+		}
+		cout << "\n";
+	}
 
 }
 
 void shellSort(vector<int>& vec) {
-    cout << "Numbers with dots. have been switched" << endl;
-    size_t n = vec.size();
-    int l = 1;
+	cout << "Numbers with dots. are compared" << endl;
+	size_t n = vec.size();
+	int l = 1;
 
-    // Start with a big gap, then reduce the gap 
-    for (int gap = n / 2; gap > 1; gap /= 2) {
-        // Do a gapped insertion sort for this gap size. 
-        // The first gap elements a[0..gap-1] are already 
-        // in gapped order keep adding one more element 
-        // until the entire vec is gap sorted 
-        for (size_t i = gap; i < n; i++) {
-            // store value 
-            int temp = vec[i];
+	// Start with the midpoint, then reduce the gap 
+	for (int gap = n / 2; gap > 1; gap /= 2) {
+	    cout << "*** gap is " << gap << " ***" << endl;
+	    for (size_t i = gap; i < n; i++) {
+			cout << "*** Iteration " << l << " ***" << endl;
+	        int temp = vec[i];
 
-            // shift earlier gap-sorted elements up until 
-            // the correct location for temp is found 
-            int j;
-            for (j = i; j >= gap && vec[j - gap] > temp; j -= gap) {
-                vec[j] = vec[j - gap];
-            }
+	        // shift earlier gap-sorted elements up until 
+	        // the correct location for temp is found 
+	        int j;
+	        for (j = i; j >= gap && vec[j - gap] > temp; j -= gap) {
+	            cout << "swapping " << vec[j] << " and " << vec[j - gap] << endl;
+	            vec[j] = vec[j - gap];
+	        }
+	        // put in correct location
+	        vec[j] = temp;
+	        
 
-            // put in correct location
-            vec[j] = temp;
-
-            cout << "Step " << l << endl;
-            for (size_t k = 0; k < vec.size(); k++) {
-                cout << vec[k];
-                if (k == j) {
-                    cout << ".";
-                }
-                if (k == i && j != i) {
-                    cout << ".";
-                }
-                cout << " ";
-            }
-            cout << "\n";
-            l++;
-        }
-    }
-    cout << "---\nInsertion Portion" << endl;
-    insertionSort(vec);
-}
-
-// Prints the vec
-void printvec(vector<int>& vec) {
-    size_t n = vec.size();
-    for (std::vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
-        std::cout << *i << ' ';
-    }
-    cout << "\n";
+			//print vector
+	        for (size_t k = 0; k < vec.size(); k++) {
+	            cout << vec[k] << " ";
+	            if (k == i - gap - gap) {
+	                cout << "\u001B[32m\b.\u001B[0m";
+	            }
+	            if (k == i - gap) {
+	                cout << "\u001B[32m\b.\u001B[0m";
+	            }
+	            if (k == i) {
+	                cout << "\u001B[32m\b.\u001B[0m";
+	            }
+	            cout << " ";
+	        }
+	        cout << "\n";
+	        l++;
+	    }
+	}
+	cout << "---\nInsertion Portion" << endl;
+	insertionSort(vec);
+	//size_t n = vec.size();
+	//for (int h = n / 2; h > 0; h = h / 2)
+	//{
+	//	cout << "gap is " << h << endl;
+	//	for (size_t unsorted = h; unsorted < n; unsorted++)
+	//	{
+	//		int nextItem = vec[unsorted];
+	//		int loc = unsorted;
+	//		while ((loc >= h) && (vec[loc - h] > nextItem))
+	//		{
+	//			cout << "swapping " << vec[loc] << " and " << vec[loc - h] << endl;
+	//			vec[loc] = vec[loc - h];
+	//			loc = loc - h;
+	//			printVector(vec);
+	//		} // end while
+	//		vec[loc] = nextItem;
+	//	}  // end for
+	//}  // end for
 }
 
 int main()
 {
-    cout << "* **********************************************" << endl
-        << "*** San Francisco State University" << endl
-        << "*** CSC 340 - Programming Methodology" << endl
-        << "*** Sorting Demonstration" << endl
-        << "* **********************************************" << endl;
-    vector<int> data = { 1, 8, 4, 0, 1, 1, 7, 6, 4, 6, 1, 7 };
-    vector<int> copy1 = data;
-    vector<int> copy2 = data;
-    cout << "---\nSelection Sort\n---" << endl;
-    cout << "Unsorted vector:" << endl;
-    printvec(data);
-    selectionSort(data);
-    cout << "Sorted vector" << endl;
-    printvec(data);
-    cout << "---\nInsertion Sort\n---" << endl;
-    cout << "Unsorted vector:" << endl;
-    printvec(copy1);
-    insertionSort(copy1);
-    cout << "Sorted vector" << endl;
-    printvec(copy1);
-    cout << "---\nShell Sort\n---" << endl;
-    cout << "Unsorted vector:" << endl;
-    printvec(copy2);
-    shellSort(copy2);
-    cout << "Sorted vector" << endl;
-    printvec(copy2);
+	cout << "* **********************************************" << endl
+		<< "*** San Francisco State University" << endl
+		<< "*** CSC 340 - Programming Methodology" << endl
+		<< "*** Sorting Demonstration" << endl
+		<< "* **********************************************" << endl;
+	vector<int> data = { 1, 8, 4, 0, 1, 1, 7, 6, 4, 6, 1, 7 };
+	vector<int> copy1 = data;
+	vector<int> copy2 = data;
+	cout << "---\nSelection Sort\n---" << endl;
+	cout << "Unsorted vector:" << endl;
+	printVector(data);
+	selectionSort(data);
+	cout << "Sorted vector:" << endl;
+	printVector(data);
+	cout << "---\nInsertion Sort\n---" << endl;
+	cout << "Unsorted vector:" << endl;
+	printVector(copy1);
+	insertionSort(copy1);
+	cout << "Sorted vector:" << endl;
+	printVector(copy1);
+	cout << "---\nShell Sort\n---" << endl;
+	cout << "Unsorted vector:" << endl;
+	printVector(copy2);
+	shellSort(copy2);
+	cout << "Sorted vector:" << endl;
+	printVector(copy2);
 }
